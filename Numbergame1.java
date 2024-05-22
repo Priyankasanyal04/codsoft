f27f2c4cdf93985e43f16e227422e213f704dd37
Priyankasanyal04 <sanyalpriyanka4@gmail.com>
2024-05-22 13:18:48 +0530

    Add files via upload

---
+++ Numbergame1.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+import java.util.Random;
+
+class Numbergame1 {
+    public static void main(String args[]) {
+        int inputno;
+        int randomno;
+        int noofguess;
+        int noofrounds = 0;
+        boolean playagain = true;
+        boolean guessiscorrect;
+
+        Scanner sc = new Scanner(System.in);
+        Random r1 = new Random();
+
+        while (playagain && noofrounds < 3) {
+            noofrounds++;
+            randomno = r1.nextInt(1, 101); // Generate a new random number each round
+            noofguess = 0; // Reset the number of guesses for each round
+            guessiscorrect = false; // Reset the guess correctness for each round
+
+            System.out.println("Round " + noofrounds + ": Guess a number between 1 and 100");
+
+            while (noofguess < 3 && !guessiscorrect) {
+                inputno = sc.nextInt();
+                noofguess++;
+
+                if (inputno == randomno) {
+                    System.out.println("Your guess is correct! You guessed it in " + noofguess + " attempts.");
+                    guessiscorrect = true;
+                } else if (inputno > randomno) {
+                    System.out.println("Your guess is too high. Try again!");
+                } else {
+                    System.out.println("Your guess is too low. Try again!");
+                }
+            }
+
+            if (!guessiscorrect) {
+                System.out.println("Sorry, you have exhausted all your attempts. The correct number was " + randomno + ".");
+            }
+
+            if (noofrounds < 3) { // Ask to play again if rounds are left
+                System.out.println("Do you want to play again (yes/no)?");
+                String userResponse = sc.next();
+                if (userResponse.equalsIgnoreCase("yes")) {
+                    playagain = true;
+                } else {
+                    playagain = false;
+                }
+            }
+        }
+
+        System.out.println("Game over");
+        sc.close();
+    }
+}
